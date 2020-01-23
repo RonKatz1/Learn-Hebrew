a000484ec48ad5c7b2556c2e270ece97e3546846
tal <tal333_d@walla.co.il>
2020-01-23 21:42:31 +0200

    creat project

---
+++ Project/LearnHebrew/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LearnHebrew.Startup))]
+namespace LearnHebrew
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
